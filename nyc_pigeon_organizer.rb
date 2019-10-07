e3041e2ec711aefce3cab806bc39fdb3cfa7da4f
Jason Hundal <jasonhundal23@gmail.com>
2019-10-07 06:49:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  myhash = {}
+  data.each do |key, value|
+    value.each do |myhash, names|
+      names.each do |name|
+        
+        if !myhash[name]
+          myhash[name] = {}
+        end
+
+        if !myhash[name][key]
+          myhash[name][key] = []
+        end
+
+        myhash[name][key] << myhash.to_s
+
+      end
+    end
+  end
+  myhash
 end
